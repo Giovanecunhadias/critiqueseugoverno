90220c1743741d3c0eafff8a0430799793eb6ab9
Giovane <98927748+Giovanecunhadias@users.noreply.github.com>
2025-03-02 22:55:55 -0300

    up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,24 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  
 });
+
+
+
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-var": "off", // Desativa a regra para `var`
+      "@typescript-eslint/no-explicit-any": "off", // Permite `any` temporariamente
+      "@typescript-eslint/no-unused-vars": ["warn"], // Emite apenas um aviso em vez de erro
+    },
+  },
 ];
+
+
+
 
 export default eslintConfig;
