19255598e3c8b3c95d9b9aa83995091774c59d56
Giovane <98927748+Giovanecunhadias@users.noreply.github.com>
2025-03-02 22:51:40 -0300

    up

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,14 @@
-import { PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+
 const db = globalThis.prisma || new PrismaClient()
 
-declare global{
+// eslint-disable-next-line no-var
+declare global {
     var prisma: PrismaClient
 }
-if(process.env.NODE_ENV !== 'production'){
+
+if (process.env.NODE_ENV !== 'production') {
     globalThis.prisma = db
 }
+
 export default db;
